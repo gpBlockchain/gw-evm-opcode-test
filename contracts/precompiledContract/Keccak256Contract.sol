8cd1df24d317ab263407a838de4321be9c1bbe99
linguopeng <linguopeng@hyperchain.cn>
2022-03-25 02:02:35 +0800

    add keccak

--- contracts/precompiledContract/Keccak256Contract.sol
+++ contracts/precompiledContract/Keccak256Contract.sol
@@ -2,6 +2,15 @@ pragma solidity ^0.8.4;
 
 contract Keccak256Contract {
 
+    bytes32 public  hashResult;
+
+    event KeccakEvent(uint256 idx,bytes data);
+    function setKeccak256(bytes memory data) public returns(bytes32 result){
+        hashResult = callKeccak256(data);
+        emit KeccakEvent(1,data);
+        return hashResult;
+    }
+
     function callKeccak256(bytes memory data) public pure returns(bytes32 result){
         return keccak256(data);
     }
