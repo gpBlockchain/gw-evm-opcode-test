5a6067902b9b437d52bfe81311a135a657b6bfd6
linguopeng <linguopeng@hyperchain.cn>
2022-03-28 15:55:08 +0800

    add issue232

--- contracts/opcodes/bug/BugContract.sol
+++ contracts/opcodes/bug/BugContract.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.8.4;
 contract BugContract{
 
 
+    function iss232Test() public view returns (bytes memory a, bytes memory b){
+        return  add(33,4,32);
+    }
+
+    function add(uint256 b2, uint256 cc1, uint256 cc2) public view returns (bytes memory a, bytes memory b){
+        assembly {
+            a := mload(0x40)
+            b := add(a, b2)
+            codecopy(a, cc1, cc2)
+        }
+    }
+
 }
---
+++ test/bugs/issue232.js
@@ -0,0 +1,24 @@
+//https://github.com/nervosnetwork/godwoken-web3/issues/232
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
+describe.only("issue232.js", function () {
+
+
+    let contract;
+
+    before(async function () {
+        const blockInfoContract = await ethers.getContractFactory("BugContract");
+        contract = await blockInfoContract.deploy();
+        await contract.deployed();
+        console.log("contractAddress:", contract.address);
+    });
+
+    it.only("issue232", async () => {
+        let result  = await contract.iss232Test();
+        console.log("result",result)
+        console.log("a:",result[0])
+        console.log("b:",result[1])
+        expect(result[0].length).to.be.equal(130)
+        expect(result[1].length).to.be.equal(81922)
+    })
+})
